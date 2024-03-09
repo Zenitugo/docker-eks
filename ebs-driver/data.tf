9c2303ae286c2f1d8064693810b49df2ab9fa0ff
zenitugo <ugochiukaegbu19@gmail.com>
2024-03-09 13:05:50 +0100

    ebs files

--- ebs-driver/data.tf
+++ ebs-driver/data.tf
@@ -3,5 +3,5 @@ data "aws_partition" "current_testing" {}
 data "aws_caller_identity" "current_identity" {}
 
 data "aws_eks_cluster" "eks-cluster" {
-  name = var.cluster_name
+  name = "my-eks-cluster"
 }
