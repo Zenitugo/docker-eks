8dd0c5cc48b7588ed7d92e962a1887a9a0a99a9c
zenitugo <ugochiukaegbu19@gmail.com>
2024-03-12 20:30:30 +0100

    ebs files

--- ebs-driver/data.tf
+++ ebs-driver/data.tf
@@ -5,3 +5,22 @@ data "aws_caller_identity" "current_identity" {}
 data "aws_eks_cluster" "eks-cluster" {
   name = "my-eks-cluster"
 }
+
+data "aws_iam_policy_document" "ebs-volume-policy" {
+  count = var.volume_count
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "ec2:AttachVolume",
+      "ec2:DetachVolume",
+    ]
+
+    resources = [
+      "arn:${data.aws_partition.current_testing.partition}:${data.aws_caller_identity.current_identity.account_id}:volume/${element(aws_ebs_volume.volumes.*.id, count.index)}",
+      "arn:${data.aws_partition.current_testing.partition}:${data.aws_caller_identity.current_identity.account_id}:instance/*",
+    ]
+  }
+}
+
+data "availability_zones" "az" {}
