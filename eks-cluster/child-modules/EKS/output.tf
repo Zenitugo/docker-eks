cc3c4d635e2f3bfc86928a75a9e03baa12ec3582
zenitugo <ugochiukaegbu19@gmail.com>
2024-03-23 11:52:51 +0100

    terraform script

--- eks-cluster/child-modules/EKS/output.tf
+++ eks-cluster/child-modules/EKS/output.tf
@@ -8,3 +8,15 @@ output "clustername" {
 output "clusteridentity" {
     value = aws_eks_cluster.eks-cluster.identity
 }
+
+
+# Output openid_connect_provider url
+output "openid-url" {
+    value = aws_iam_openid_connect_provider.eks.url
+}
+
+
+# Output openid_connect_provider arn
+output "openid-arn" {
+    value = aws_iam_openid_connect_provider.eks.arn
+}
