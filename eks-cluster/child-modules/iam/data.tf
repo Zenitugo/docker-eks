134c9a93c9731da63b417d59ef5305dfdb60b3e4
zenitugo <ugochiukaegbu19@gmail.com>
2024-03-23 11:57:09 +0100

    terraform iam file

---
+++ eks-cluster/child-modules/iam/data.tf
@@ -0,0 +1,38 @@
+#Query AWS for the iam policy document for ebs-csi-addon
+data "aws_iam_policy_document" "csi" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub"
+      values   = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
+    }
+
+    principals {
+      identifiers = [aws_iam_openid_connect_provider.eks.arn]
+      type        = "Federated"
+    }
+  }
+}
+
+
+#Query AWS fot the iam policy document for aws nginx-ingress load balancer controller
+data "aws_iam_policy_document" "controller" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(var.openid-url, "https://", "")}:sub"
+      values   = ["system:serviceaccount:kube-system:aws-load-balancer-controller"]
+    }
+
+    principals {
+      identifiers = [var.openid-arn]
+      type        = "Federated"
+    }
+  }
+}
