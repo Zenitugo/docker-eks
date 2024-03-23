0624363631149164e8b8983b4e5be30fb6109e66
zenitugo <ugochiukaegbu19@gmail.com>
2024-03-23 12:01:05 +0100

    iam terraform file

--- eks-cluster/child-modules/iam/variable.tf
+++ eks-cluster/child-modules/iam/variable.tf
@@ -1,3 +1,8 @@
 variable "cluster-rolename" {}
 variable "node-role-name" {}
+variable "role_name" {}
+variable "openid-url" {}
+variable "openid-arn" {}
+variable "controller_role_name" {}
+variable "controller_policy_name" {}
 variable "clustername" {}
