786d2cb9b3e136c47afc707a144526c9b283a259
zenitugo <ugochiukaegbu19@gmail.com>
2024-03-23 11:52:07 +0100

    terraform script

--- eks-cluster/child-modules/EKS/data.tf
+++ eks-cluster/child-modules/EKS/data.tf
@@ -1,21 +1,14 @@
+#Query AWS infrastructure for the provider url generated when the eks was created
 data "tls_certificate" "eks" {
   url = aws_eks_cluster.eks-cluster.identity[0].oidc[0].issuer
 }
 
-data "aws_iam_policy_document" "csi" {
-  statement {
-    actions = ["sts:AssumeRoleWithWebIdentity"]
-    effect  = "Allow"
 
-    condition {
-      test     = "StringEquals"
-      variable = "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub"
-      values   = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
-    }
 
-    principals {
-      identifiers = [aws_iam_openid_connect_provider.eks.arn]
-      type        = "Federated"
-    }
-  }
+
+# Query the aws for the latest addon version for the eks
+data "aws_eks_addon_version" "latest" {
+  addon_name         = var.addon_name
+  kubernetes_version = aws_eks_cluster.eks-cluster.version
+  most_recent        = true
 }
